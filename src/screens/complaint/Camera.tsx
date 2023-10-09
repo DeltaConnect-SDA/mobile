541f025fe762a27a95f13dbe25a7decf885aa616
yukebrillianth <yukebrilliant@gmail.com>
2023-10-10 00:17:36 +0700

    Fix: change to class component in camera component

--- src/screens/complaint/Camera.tsx
+++ src/screens/complaint/Camera.tsx
@@ -1,5 +1,13 @@
-import { StyleSheet, Text, View, Alert, TouchableOpacity, Dimensions } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TouchableOpacity,
+  Dimensions,
+  ActivityIndicator,
+} from 'react-native';
+import React, { useState, useEffect, Component } from 'react';
 import {
   Camera as ExpoCamera,
   CameraType,
@@ -9,88 +17,119 @@ import {
 } from 'expo-camera';
 import { CameraFilled, CameraFlip, FlashFilled, FlashOutline, Torch } from '@/constants/icons';
 import { Colors } from '@/constants/colors';
-import { useIsFocused } from '@react-navigation/native';
 
 type Props = {};
-const Camera: React.FC = () => {
-  const [type, setType] = useState<CameraType>(CameraType.back);
-  const [flash, setFlash] = useState<FlashMode>(FlashMode.off);
 
-  useEffect(() => {
-    requestPermissions();
-  }, []);
+interface State {
+  granted: boolean;
+  type: CameraType;
+  flash: FlashMode;
+  isLoading: boolean;
+}
 
-  const requestPermissions = async () => {
-    await requestCameraPermissionsAsync();
-  };
-
-  const getPermissions = async () => {
-    const cameraPermission = await getCameraPermissionsAsync();
-
-    return cameraPermission.granted;
-  };
-
-  console.log(getCameraPermissionsAsync());
-
-  console.log('ok');
-
-  if (!getPermissions()) {
-    return Alert.alert(
-      'Perizinan dibutuhkan!',
-      'Anda harus mengizinkan penggunaan kamera untuk melapor.',
-      [{ text: 'Baik' }]
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <ExpoCamera style={styles.camera} type={type} flashMode={flash} ratio="4:3"></ExpoCamera>
-        <View style={styles.controllerContainer}>
-          <TouchableOpacity
-            onPress={() => setType(type === CameraType.back ? CameraType.front : CameraType.back)}
-            style={{ backgroundColor: '#606060', padding: 20, borderRadius: 100 }}>
-            <CameraFlip />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: Colors.PRIMARY_GREEN,
-              width: 85,
-              height: 85,
-              borderRadius: 150,
-              borderWidth: 10,
-              borderColor: 'white',
-            }}>
-            <CameraFilled />
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() =>
-              setFlash(
-                flash === FlashMode.off
-                  ? FlashMode.on
-                  : flash === FlashMode.torch
-                  ? FlashMode.off
-                  : FlashMode.torch
-              )
-            }
-            style={{ backgroundColor: '#606060', padding: 20, borderRadius: 100 }}>
-            {flash === FlashMode.off ? (
-              <FlashOutline />
-            ) : flash === FlashMode.on ? (
-              <FlashFilled />
-            ) : flash === FlashMode.torch ? (
-              <Torch />
-            ) : (
-              <FlashOutline />
-            )}
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
+export class Camera extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { granted: false, type: CameraType.back, flash: FlashMode.off, isLoading: true };
   }
-};
 
+  async componentDidMount() {
+    const { granted } = await requestCameraPermissionsAsync();
+    console.log(granted);
+
+    if (granted === true) {
+      this.setState({ granted: true });
+      this.setState({ isLoading: false });
+    } else {
+      Alert.alert(
+        'Perizinan dibutuhkan!',
+        'Anda harus mengizinkan penggunaan kamera untuk melapor.',
+        [{ text: 'Baik' }]
+      );
+      this.setState({ isLoading: false });
+      this.setState({ granted: false });
+    }
+  }
+
+  render() {
+    if (!this.state.isLoading) {
+      if (!this.state.granted) {
+        return <Text>wah parah</Text>;
+      } else {
+        return (
+          <View style={styles.container}>
+            <ExpoCamera
+              style={styles.camera}
+              type={this.state.type}
+              flashMode={this.state.flash}
+              ratio="4:3"></ExpoCamera>
+            <View style={styles.controllerContainer}>
+              <TouchableOpacity
+                onPress={() =>
+                  this.setState({
+                    type: this.state.type === CameraType.back ? CameraType.front : CameraType.back,
+                  })
+                }
+                style={{ backgroundColor: '#606060', padding: 20, borderRadius: 100 }}>
+                <CameraFlip />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: Colors.PRIMARY_GREEN,
+                  width: 85,
+                  height: 85,
+                  borderRadius: 150,
+                  borderWidth: 10,
+                  borderColor: 'white',
+                }}>
+                <CameraFilled />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() =>
+                  this.setState({
+                    flash:
+                      this.state.flash === FlashMode.off
+                        ? FlashMode.on
+                        : this.state.flash === FlashMode.torch
+                        ? FlashMode.off
+                        : FlashMode.torch,
+                  })
+                }
+                style={{ backgroundColor: '#606060', padding: 20, borderRadius: 100 }}>
+                {this.state.flash === FlashMode.off ? (
+                  <FlashOutline />
+                ) : this.state.flash === FlashMode.on ? (
+                  <FlashFilled />
+                ) : this.state.flash === FlashMode.torch ? (
+                  <Torch />
+                ) : (
+                  <FlashOutline />
+                )}
+              </TouchableOpacity>
+            </View>
+          </View>
+        );
+      }
+    } else {
+      return (
+        <View
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+            gap: 10,
+          }}>
+          <ActivityIndicator size="large" color={Colors.PRIMARY_GREEN} />
+          <Text>Tunggu Sebentar.....</Text>
+        </View>
+      );
+    }
+  }
+}
 export default Camera;
 
 const styles = StyleSheet.create({
